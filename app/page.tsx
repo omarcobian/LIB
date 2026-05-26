33e10baaf72b5cb27525990735a76d58bfc116e6
Omar <omar.cobian8228@alumnos.udg.mx>
2026-05-25 22:52:35 -0600

    feat: add logging for Supabase URL and anon key in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,8 @@ type MateriaAntiguaRaw = {
 };
 
 export default async function HomePage() {
+  console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
   const supabase = await createClient();
 
   const { data: antiguas } = await supabase
